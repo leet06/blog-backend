ef44fe0633a490ff9fb48076195f79823c945095
leet06 <beresnev2006@gmail.com>
2026-07-09 04:58:23 +0300

    feat: implement endpoint for fetching authenticated user posts

---
+++ app/Http/Controllers/Api/MyPostController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+use App\Http\Requests\Api\GetPostsRequest;
+use App\Services\PostService;
+use Illuminate\Http\JsonResponse;
+
+class MyPostController extends Controller
+{
+    public function __invoke(GetPostsRequest $request, PostService $postService): JsonResponse
+    {
+        // Validation of input parameters (limit, offset, sorting, dates)
+        $params = $request->validated();
+        
+        // Current authorized user
+        $user = $request->user();
+
+        // Get an array from the class
+        $posts = $postService->getUserFilteredList($user, $params);
+
+        return response()->json([
+            'posts' => $posts,
+            'count' => count($posts),
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\Api\IndexPostController;
+use App\Http\Controllers\Api\MyPostController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -13,5 +14,10 @@ use App\Http\Controllers\Api\IndexPostController;
 
 Route::post('/login', LoginController::class);
 Route::post('/register', RegisterController::class);
-Route::middleware('auth:sanctum')->post('/posts', PostController::class);
 Route::get('/posts', IndexPostController::class);
+
+Route::middleware('auth:sanctum')->group(function ()
+{
+    Route::post('/posts', PostController::class);
+    Route::get('/posts/my', MyPostController::class);
+});
